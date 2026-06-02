0cae80dff69e860f25a8323564c457c354bf7bac
Bobby Prins <bobby.prins@gmail.com>
2026-06-02 21:36:32 +0200

    chore: make sure brew keeps working on MacOS

--- home/darwin.nix
+++ home/darwin.nix
@@ -5,6 +5,8 @@
     homeDirectory = "/Users/bprins";
   };
 
+  programs.zsh.profileExtra = "eval $(/opt/homebrew/bin/brew shellenv)";
+
   # MacOS package is currently not available.
   # Only manage Ghostty configuration.
   programs.ghostty.package = null;
