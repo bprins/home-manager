680a0bc4293f52fc2975c822db155ba8db4aa9d1
Bobby Prins <bobby.prins@gmail.com>
2026-06-02 21:20:19 +0200

    feat: add claude-code for MacOS

--- home/darwin.nix
+++ home/darwin.nix
@@ -8,4 +8,9 @@
   # MacOS package is currently not available.
   # Only manage Ghostty configuration.
   programs.ghostty.package = null;
+
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "claude-code"
+  ];
+  programs.claude-code.enable = true;
 }
