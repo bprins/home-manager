61e04988b75810a3615ff7b07505f2ef04343467
Bobby Prins <bobby.prins@gmail.com>
2026-07-28 14:14:51 +0200

    feat: add gh to the darwin extra profile

--- home/darwin-extra.nix
+++ home/darwin-extra.nix
@@ -6,5 +6,14 @@
 
   programs.claude-code.enable = true;
 
+  programs.gh = {
+    enable = true;
+    settings.git_protocol = "https";
+    gitCredentialHelper = {
+      enable = true;
+      hosts = [ "https://github.com" "https://gist.github.com" ];
+    };
+  };
+
   home.packages = [ pkgs.obsidian ];
 }
