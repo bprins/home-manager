515630dd096e81f1533913dfa2e63ae1dedf9aba
Bobby Prins <bobby.prins@gmail.com>
2026-09-08 14:23:45 +0200

    feat: add caffeine on macos

--- home/darwin.nix
+++ home/darwin.nix
@@ -1,12 +1,19 @@
-{ lib, ... }: {
+{ lib, pkgs, ... }: {
   imports = [ ./common.nix ];
 
   home = {
     homeDirectory = lib.mkDefault "/Users/bprins";
+
+    packages = with pkgs; [
+      caffeine
+    ];
   };
 
   # Defaults back to true below stateVersion 25.11, we use copyApps
   targets.darwin.linkApps.enable = false;
+
+  # Make sure Finder and Spotlight see home-manager installed Mac apps
+  targets.darwin.copyApps.enable = true;
 
   programs.zsh.profileExtra = ''
     if [ -x /opt/homebrew/bin/brew ]; then
@@ -14,7 +21,6 @@
     fi
   '';
 
-  # MacOS package is currently not available.
-  # Only manage Ghostty configuration.
+  # MacOS package is currently not available; only manage Ghostty configuration
   programs.ghostty.package = null;
 }
--- home/profiles/notes.nix
+++ home/profiles/notes.nix
@@ -2,7 +2,4 @@
   home.packages = with pkgs; [
     obsidian
   ];
-
-  # darwin-only: without it obsidian is only a store path
-  targets.darwin.copyApps.enable = true;
 }
