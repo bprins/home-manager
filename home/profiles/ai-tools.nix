e7c06085104c23fb1aff447eb5a40cd04c583ead
Bobby Prins <bobby.prins@gmail.com>
2026-09-25 15:43:22 +0200

    feat: openshell osh function

--- home/profiles/ai-tools.nix
+++ home/profiles/ai-tools.nix
@@ -1,3 +1,8 @@
 { ... }: {
   programs.claude-code.enable = true;
+
+  # osh <sandbox> [command...]: SSH into an OpenShell sandbox
+  programs.zsh.initContent = ''
+    osh() { ssh -t -F =(openshell sandbox ssh-config "$1") "openshell-$1.default" "''${@:2}"; }
+  '';
 }
